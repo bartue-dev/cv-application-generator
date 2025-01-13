097ccaf8e4c5db72cb23dfb8eec158591c9bb94d
Bartue <[bartbatiancila28@gmail.com]>
2025-01-13 23:36:42 +0800

    Import the FormComponent as well as the OutputComponent

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import './App.css'
+import FormComponent from "./Components/FormComponent/FormComponent"
+import OutputComponent from "./Components/OutputComponent/OutputComponent"
 
 function App() {
 
   return (
     <>
-     
+    <div className="app-container">
+      <FormComponent />
+      <OutputComponent />
+    </div>
     </>
   )
 }
