2274baaab0a091a16a99c5072d26cde27464cfde
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-01-17 00:47:30 +0800

    Add a form compoent where the two form located add form and update form

--- src/Components/WorkInfo/WorkInfo.jsx
+++ src/Components/WorkInfo/WorkInfo.jsx
@@ -1,8 +1,17 @@
 import "./WorkInfo.css";
 import Icon from '@mdi/react';
-import { mdiChevronDown } from '@mdi/js';
+import { mdiChevronDown, mdiPlus, mdiPencil } from '@mdi/js';
+import { useState } from "react";
+import { WorkInfoForm, EditWOrkInfoForm} from "./WorkInfoForm";
 
 function WorkInfo({ isActive, onShow }) {
+  const [showForm, setShowForm] = useState(false);
+  const [workInfoData, setWorkInfoData] = useState([]);
+  const [isEditForm, setIsEditForm] = useState(0);
+
+  // console.log(isEditForm);
+  // console.log("Work info Data", workInfoData)
+
   return (
     <div className="work-experience-info">
       <div className="heading-container">
@@ -10,38 +19,61 @@ function WorkInfo({ isActive, onShow }) {
         <Icon path={mdiChevronDown} size={1} className="down-arrow" onClick={onShow} style={{display: isActive ? "none" : "block"}}/>
       </div>
       
-      {isActive ? ( <form className="input-container">
-        <label>
-          Company
-          <input type="text" name="company" id="company-id"/>
-        </label>
+      {isActive && (
+        <>
 
-        <label>
-          Position
-          <input type="text" name="position" id="position-id"/>
-          </label>
+          {workInfoData.map((info) => (
 
-        <label htmlFor="description-id">Description</label>
-        <textarea name="description" id="description-id"></textarea>
+              <div
+              className="work-info-list"
+              key={info.id}
+              >
 
-        <label>
-          Year
-          <input type="text" name="year" id="year-id"/>
-        </label>
+              {isEditForm === info.id ?
+               <EditWOrkInfoForm setIsEditForm={setIsEditForm}/> :
 
-        <label>
-          Location
-          <input type="text" name="location" id="location-id"/>
-        </label>
+              <>
+                  <div className="title-container">
+                    <h2 className="work-info-title1">{info.company}</h2>
+                    <h3 className="work-info-title2"> &#183; {info.position}</h3>
+                  </div>
 
-        <div className="button-container">
-          <button className="delete-button">Delete</button>
-          <div className="button-item">
-            <button className="cancel-button">Cancel</button>
-            <button className="submit-button">Submit</button>
-          </div>
-        </div>
-      </form>) : ("")}
+                  <button
+                    className="edit-button"
+                    onClick={() => setIsEditForm(info.id)}
+                  >
+                    <Icon path={mdiPencil} size={1} />
+                  </button> 
+
+               </>
+
+              }
+
+              
+                    
+            </div>
+            ))
+          }          
+
+          {
+            showForm ? 
+            <WorkInfoForm
+             onCancel={() => setShowForm(false)}
+             workInfoData={workInfoData}
+             setWorkInfoData={setWorkInfoData}
+            /> :
+            <div className="add-work-experience-container">
+  
+              <h2>Add Work Experience</h2>
+              
+              <button onClick={() => setShowForm(true)}>
+                <Icon path={mdiPlus} size={1} className="add-icon" />
+              </button>
+  
+            </div>
+          }
+        </>
+      )}
      
     </div>
   )
