40d955554f7da90be3cc0d18b53573d9b3eb6d61
Bartue <[bartbatiancila28@gmail.com]>
2025-01-21 18:40:34 +0800

    Add a personalInfoData state where the personal info data stores while use typing each input. Import the appContext the use the useContext hook to get the personalInfoData and pass it to the output component

--- src/Components/PersonalInfo/PersonalInfo.jsx
+++ src/Components/PersonalInfo/PersonalInfo.jsx
@@ -1,9 +1,34 @@
 import "./PersonalInfo.css";
 import Icon from '@mdi/react';
 import { mdiChevronDown } from '@mdi/js';
+import { appContext } from "../utils/AppContext";
+import { useContext, useState } from "react";
 
-function PersonalInfo({ isActive, onShow }) {
+function PersonalInfo({ isActive, onShow}) {
+  const context = useContext(appContext)
 
+  const [personalInfoData, setPersonalInfoData] = useState({
+    name: "",
+    email: "",
+    phone: "",
+    portfolio: ""
+  });
+
+  //console.log("personal info data",personalInfoData);
+  
+  function handleInputChange(e) {
+
+    const newData = {
+      ...personalInfoData,
+      [e.target.name] : e.target.value
+    }
+
+    setPersonalInfoData(newData)
+
+    context.updatedPersonalData(newData)
+  }
+
+  
   return (
     <div className="personal-info">
       <div className="heading-container">
@@ -15,27 +40,59 @@ function PersonalInfo({ isActive, onShow }) {
 
       {
       isActive && ( 
-      <form className="input-container">
+      <form 
+        className="input-container"
+      >
         <h2>Add Personal Info</h2>
         
         <label>
           Name
-          <input type="text" name="name" id="name-id"/>
+          <input 
+          type="text" 
+          name="name" 
+          id="name-id"
+          value={personalInfoData.name}
+          onChange={(e) => 
+            handleInputChange(e)
+          } 
+          />
+          
         </label>
 
         <label>
           Email
-          <input type="email" name="email" id="email-id"/>
+          <input 
+          type="email" 
+          name="email" 
+          id="email-id"
+          value={personalInfoData.email}
+          onChange={(e) => handleInputChange(e)}
+          />
+          
         </label>
 
         <label>
           Phone #
-          <input type="tel" name="phone" id="phone-id"/>
+          <input 
+          type="tel" 
+          name="phone" 
+          id="phone-id"
+          value={personalInfoData.phone}
+          onChange={(e) => handleInputChange(e)}
+          />
+          
         </label>
 
         <label>
           Portfolio
-          <input type="tel" name="portfolio" id="portfolio-id"/>
+          <input 
+          type="tel" 
+          name="portfolio" 
+          id="portfolio-id"
+          value={personalInfoData.portfolio}
+          onChange={(e) => handleInputChange(e)}
+          />
+          
         </label>
 
       </form> ) 
