2ddf37d78cd32bf6b6e88cc9386bc0aa90df7903
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-01-21 18:37:43 +0800

    Add a initial rendering of data from personalInfo component to the cv output

--- src/Components/OutputComponent/OutputComponent.jsx
+++ src/Components/OutputComponent/OutputComponent.jsx
@@ -1,8 +1,16 @@
 import "./OutputComponent.css"
 
-function OutputComponent() {
+function OutputComponent({personalData}) {
   return (
     <div className="output-component">
+
+      <div className="cv-container">
+        <h1>{personalData.name}</h1>
+        <h1>{personalData.email}</h1>
+        <h1>{personalData.phone}</h1>
+        <h1>{personalData.portfolio}</h1>
+      </div>
+
     </div>
   )
 }
