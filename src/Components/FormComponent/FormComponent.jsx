0cdb7253c6d50d4a7a56b3c878ed5a46567bed5f
Bartue <[bartbatiancila28@gmail.com]>
2025-01-21 18:35:59 +0800

    Declare a 3 useState state, inputValue where it store the value of the input form the customListInfo after use create a custom info form. customInfoList where the inputValue, id and customActiveIndex. customActiveIndex state is where store the active index value for each of the custom list this can be use for accordion also declare it so it could start is firstValue to 3 and can get along with outside of the list components. The use the customInfoList state to render the customList component in map method

--- src/Components/FormComponent/FormComponent.jsx
+++ src/Components/FormComponent/FormComponent.jsx
@@ -2,18 +2,51 @@ import "./FormComponent.css"
 import PersonalInfo from "../PersonalInfo/PersonalInfo";
 import EducationInfo from "../EducationInfo/EducationInfo";
 import WorkInfo from "../WorkInfo/WorkInfo"
-import AddMoreInfo from "../AddMoreInfo/AddMoreInfo"
+import CustomInfo from "../AddMoreInfo/CustomInfo"
+import CustomList from "./CustomList";
 import { useState } from "react";
 
 function FormComponent() {
   const [activeIndex, setActiveIndex] = useState(0);
+  const [inputValue, setInputValue ] = useState("");
+  const [customInfoList, setCustomInfoList] = useState([]);
+  const [customActiveIndex, setCustomActiveIndex] = useState(3);
+
+  console.log("custom list", customInfoList);
 
   return (
     <div className="form-component">
-        <PersonalInfo isActive={activeIndex === 0} onShow={() => setActiveIndex(0)}/>
-        <EducationInfo isActive={activeIndex === 1} onShow={() => setActiveIndex(1)} />
-        <WorkInfo isActive={activeIndex === 2} onShow={() => setActiveIndex(2)}/>
-        <AddMoreInfo />
+        <PersonalInfo 
+          isActive={activeIndex === 0} 
+          onShow={() => setActiveIndex(0)}
+          />
+        <EducationInfo 
+          isActive={activeIndex === 1} 
+          onShow={() => setActiveIndex(1)} />
+        <WorkInfo 
+          isActive={activeIndex === 2} 
+          onShow={() => setActiveIndex(2)}/>
+
+        {customInfoList.map((list, index) => (
+           <CustomList 
+            key={list.id}
+            {...list}
+            index={index}
+            customInfoList={customInfoList}
+            setCustomInfoList={setCustomInfoList}
+            isActive={activeIndex === list.customActiveIndex}
+            onShow={() => setActiveIndex(list.customActiveIndex)}
+            />
+        ))}
+
+        <CustomInfo 
+          inputValue={inputValue} 
+          setInputValue={setInputValue} 
+          customInfoList={customInfoList}
+          setCustomInfoList={setCustomInfoList} 
+          customActiveIndex={customActiveIndex}
+          setCustomActiveIndex={setCustomActiveIndex}
+          />
     </div>
   )
 }
