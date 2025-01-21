ff21fb3a2c6f70c199ff7ec100afe9f7301bcd3d
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-01-21 18:16:43 +0800

    Import the appContext that is created from the utils. Use it to wrap the FormComponent with appContext.Provider. Then added a state to store the data that will be take from the other child component of FormComponent. This will be achieve to use a call function for setState function

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
 import './App.css'
+import { useState } from 'react'
 import FormComponent from "./Components/FormComponent/FormComponent"
 import OutputComponent from "./Components/OutputComponent/OutputComponent"
+import { appContext } from './Components/utils/AppContext'
+
 
 function App() {
+  const [getPersonalData, setGetPersonalData] = useState([]);
+
+  const updatedPersonalData = (data) => {
+    setGetPersonalData(data)
+  }
+
+  console.log("Personal Data App comp:", getPersonalData);
 
   return (
     <>
     <div className="app-container">
-      <FormComponent />
-      <OutputComponent />
+      <appContext.Provider value={{updatedPersonalData}}>
+        <FormComponent />
+      </appContext.Provider>
+      <OutputComponent personalData={getPersonalData} />
     </div>
     </>
   )
