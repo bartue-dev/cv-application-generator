2f2b41f8ceceb8ef91c5de24cb91f81e2901616c
Bartue <[bartbatiancila28@gmail.com]>
2025-01-26 20:34:00 +0800

    Add the data from the child component and display it to the output component

--- src/Components/OutputComponent/OutputComponent.jsx
+++ src/Components/OutputComponent/OutputComponent.jsx
@@ -1,15 +1,100 @@
 import "./OutputComponent.css"
 
-function OutputComponent({personalData}) {
+function OutputComponent({
+  personalData,
+  educationData,
+  workData,
+  customTitle,
+  customListData
+}) {
   return (
     <div className="output-component">
 
       <div className="cv-container">
-        <h1>{personalData.name}</h1>
-        <h1>{personalData.email}</h1>
-        <h1>{personalData.phone}</h1>
-        <h1>{personalData.portfolio}</h1>
+        
+        <div className="personal-info-output">
+          <h1 className="name"> {personalData.name} </h1>
+          <h2 className="phone"> {personalData.phone} </h2>
+          <h2 className="address"> {personalData.address} </h2>
+          <h2 className="email"> <u>{personalData.email} </u> </h2>
+        </div>
+
+      <div className="education-info-output" >
+        <div className="title-container">
+            <h1>Education</h1>
+            <hr /> 
+        </div>
+        {educationData.map((list) => (
+            <div className="education-list" key={list.id}>   
+                <div className="left">
+                  <p> <b> {list.school} </b> </p>
+                  <p> {list.course} </p>
+                </div>
+                <div className="right">
+                  <p> {list.year} </p>
+                  <p> {list.latinHonors} </p>
+                </div>
+                <div className="last">
+                  <p> {list.description} </p>
+              </div>
+            </div>
+        ))}
       </div>
+ 
+      <div className="work-info-output" >
+        <div className="title-container">
+          <h1>Work Experience</h1>
+          <hr />
+        </div>
+        {workData.map(list => (
+            <div className="work-list" key={list.id}>
+              <div className="left">
+                <p> <b> {list.company} </b> </p>
+                <p> {list.location} </p>
+              </div>
+              <div className="right">
+                <p> {list.position} </p>
+                <p> {list.year} </p>
+              </div>
+              <div className="last">
+                <p> {list.description} </p>
+              </div>
+            </div>
+        ))}
+      </div>
+
+      {customTitle.map((titleList) => {
+      return (
+      
+        <div className="custom-list-info" key={titleList.id}>
+          <div className="title-container">
+            <h1> {titleList.title} </h1>
+            <hr />  
+          </div>
+        
+          {customListData
+          .filter(list =>  list.customTitle === titleList.title ) 
+          .map(list => (
+            <div className="custom-list" key={list.id}>
+              <div className="left">
+                <p> <b> {list.infoOne} </b> </p>
+                <p> {list.infoTwo} </p>
+              </div>
+              <div className="right">
+                <p> {list.infoThree} </p>
+                <p> {list.infoFour} </p>
+              </div>
+              <div className="last">
+                <p> {list.description} </p>
+              </div>
+          </div> 
+          ))}
+
+        </div>
+                
+      )})}
+        
+      </div> 
 
     </div>
   )
