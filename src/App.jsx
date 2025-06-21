83ddd555c8863603c9e258e36feb844462ed037b
Bartue <[bartbatiancila28@gmail.com]>
2025-06-21 17:17:37 +0800

    Remove the states becuase this states is in the context. Then remove the state that is pass as props

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,9 @@
 import './App.css'
-import { useState } from 'react'
 import FormComponent from "./Components/FormComponent/FormComponent"
 import OutputComponent from "./Components/OutputComponent/OutputComponent"
-import { appContext } from './Components/utils/AppContext'
 
 
 function App() {
-  const [getPersonalData, setGetPersonalData] = useState([]);
-  const [getEducationData, setGetEducationData] = useState([]);
-  const [getWorkData, setGetWorkData] = useState([]);
-  const [getCustomTitle, setGetCustomTitle] = useState([]);
-  const [getCustomListData, setGetCustomListData] = useState([]);
 
   // console.log("customTitle Data:", getCustomTitle);
   // console.log("custom list Data:", getCustomListData);
@@ -18,23 +11,8 @@ function App() {
 
   return (
     <div className="app-container">
-      <appContext.Provider value={{
-        setGetPersonalData,
-        setGetEducationData,
-        setGetWorkData,
-        setGetCustomTitle,
-        setGetCustomListData
-        }}>
-        <FormComponent />
-      </appContext.Provider>
-
-      <OutputComponent 
-        personalData={getPersonalData}
-        educationData={getEducationData}
-        workData={getWorkData}
-        customTitle={getCustomTitle}
-        customListData={getCustomListData}
-      />
+      <FormComponent />
+      <OutputComponent/>
     </div>
   )
 }
