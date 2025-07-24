552a3c24f6a7087b36c0e647346acda337708229
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-07-24 22:36:27 +0800

    Creates a data file that holds the static data as sample data

---
+++ src/Components/data/data.js
@@ -0,0 +1,58 @@
+
+export const personal= {
+      name: "Light Yagami",
+      email: "lightyagami@gmail.com",
+      phone: "09105359425",
+      address: "Tokyo, Japan"
+  }
+
+export const education = [
+  {
+    id: 1,
+    school: "Harvard University",
+    course: "Computer Science",
+    description: "I graduated with a degree in Computer Science from Harvard University, where I developed a solid foundation in software engineering, algorithms, systems design, and emerging technologies. My academic experience was enriched by hands-on projects, research, and collaborative work with diverse teams. I'm now focused on using these skills to build impactful, real-world solutions in the tech industry.",
+    year: "2018-2022",
+    latinHonors: "Cum Laude"
+  }
+]
+
+export const workInfo = [
+  {
+    id: 1,
+    company: "Google",
+    position: "Software Engineer",
+    description: "I’m a Software Engineer at Google, where I design and develop scalable, high-impact solutions that serve millions of users worldwide. My work involves collaborating with cross-functional teams to build reliable systems, optimize performance, and drive innovation across products. At Google, I continue to grow my expertise in large-scale infrastructure, clean code architecture, and emerging technologies, while contributing to a culture of excellence, learning, and impact.",
+    year: "2022-2024",
+    location: "United State of America"
+  }
+]
+
+export const customTitle = [
+  {
+    id: 1,
+    customActiveIndex: 3,
+    title: "Projects",
+    customListData: [
+      {
+        id: 1,
+        description: "A full-stack map application with geolocation, place search, directions API and custom markers. ",
+        infoOne: "Google maps Clone",
+        infoTwo: "Personal Project",
+        infoThree: "",
+        infoFour: ""
+      },
+      {
+        id: 1,
+        description: "A searchable note=-sharing platform for university students with document uploads, tag filtering and comment threads.",
+        infoOne: "Harvard Notes",
+        infoTwo: "Personal Project",
+        infoThree: "",
+        infoFour: ""
+      }
+    ]
+  }
+]
+
+
+
